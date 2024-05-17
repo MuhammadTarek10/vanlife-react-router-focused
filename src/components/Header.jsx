a57484f085bde8007a5110ccf86591ead5427860
Muhammad Tarek <mohammadtarek000@gmail.com>
2024-05-17 19:14:44 +0300

    login with react-router states and action

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,29 +1,39 @@
 import { Link, NavLink } from "react-router-dom";
 
+import avatarImage from "../assets/images/avatar-icon.png";
+
 export const Header = () => {
+  function fakeLogout() {
+    localStorage.removeItem("loggedIn");
+  }
+
   return (
     <header>
       <nav>
         <Link to=".">
           <h1>#VANLIFE</h1>
         </Link>
-        <div className="links">
-          <NavLink
-            className={({ isActive }) => (isActive ? "active-link" : null)}
-            to="/host">
-            Host
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => (isActive ? "active-link" : null)}
-            to="/about">
-            About
-          </NavLink>
-          <NavLink
-            className={({ isActive }) => (isActive ? "active-link" : null)}
-            to="/vans">
-            Vans
-          </NavLink>
-        </div>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          to="/host">
+          Host
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          to="/about">
+          About
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "active-link" : null)}
+          to="/vans">
+          Vans
+        </NavLink>
+        <Link to="/login">
+          <img src={avatarImage} alt="User" className="login-icon" />
+        </Link>
+        <button className="van-type clear-filters" onClick={fakeLogout}>
+          Logout
+        </button>
       </nav>
     </header>
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,10 @@
 /* eslint-disable react-refresh/only-export-components */
-import { useState } from "react";
-import { Form, useLoaderData } from "react-router-dom";
+import {
+  Form,
+  useActionData,
+  useLoaderData,
+  useNavigation,
+} from "react-router-dom";
 import { loginUser } from "../data/api";
 import { redirect } from "../utils/redirect";
 
@@ -13,27 +17,31 @@ export async function action({ request }) {
   const email = formData.get("email");
   const password = formData.get("password");
 
-  await loginUser({ email, password });
-  localStorage.setItem("loggedIn", true);
+  try {
+    await loginUser({ email, password });
+    localStorage.setItem("loggedIn", true);
 
-  return redirect("/host");
+    return redirect("/host");
+  } catch (e) {
+    return e.message;
+  }
 }
 
 export const Login = () => {
   const message = useLoaderData();
-  const [status, setStatus] = useState("idle");
-  const [error, setError] = useState(null);
+  const { state } = useNavigation();
+  const errorMessage = useActionData();
 
   return (
     <div className="login-container">
       <h1>Sign in</h1>
       {message && <h3 className="login-error">{message}</h3>}
-      {error && <h3>{error.message}</h3>}
-      <Form method="POST" className="login-form">
+      {errorMessage && <h3>{errorMessage}</h3>}
+      <Form method="POST" className="login-form" replace>
         <input type="email" name="email" placeholder="Enter Email" />
         <input type="password" name="password" placeholder="Enter Password" />
-        <button disabled={status === "submitting"}>
-          {status === "submitting" ? "Logging..." : "Login"}
+        <button disabled={state === "submitting"}>
+          {state === "submitting" ? "Logging..." : "Login"}
         </button>
       </Form>
     </div>
